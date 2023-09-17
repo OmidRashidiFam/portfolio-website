7699195f78051f87dbff223467f3e66f14e7752d
OmidRashidiFam <omidRashidiFam@gmail.com>
2023-09-17 13:58:37 +0330

    about section done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Nav />
+      <About />
     </div>
   );
 }
